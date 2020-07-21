6b1a9217701bf0e37e1e333c045223edb22fbdbb
Relirk <olverajunior2014@gmail.com>
2020-07-21 20:19:56 -0300

    First Heroku deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ import app from './app';
 const serverInstance = app.listen(process.env.PORT, () => {
   const host = serverInstance.address().address;
   const { port } = serverInstance.address();
-  console.log('API listening at http://%s:%s', host, port);
+  console.log('Portfolio API listening at http://%s:%s', host, port);
 });
