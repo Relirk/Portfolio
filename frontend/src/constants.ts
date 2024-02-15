7ab0b25c5d42d980c55edd110e01d0e695d427f5
Francisco Olvera <francisco.olvera@mercadolivre.com>
2024-02-15 00:43:18 -0300

    feat: add config constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,11 @@
+const firebaseBasePath =
+  "https://firebasestorage.googleapis.com/v0/b/portifolio-nextjs-4ab5d.appspot.com/o/";
+
+export const staticUrlImages = {
+  aboutBackgroundImage: `${firebaseBasePath}background.jpg?alt=media&token=1cd3772d-267c-4c6a-9025-a3dfa7632c34`,
+};
+
+export const staticUrlDocuments = {
+  email: "mailto:olverajunior2014@gmail.com",
+  cv: `${firebaseBasePath}cv-folvera-linkedin.pdf?alt=media&token=39cfb1b0-a77d-4996-9145-c85227137eb7`,
+};
