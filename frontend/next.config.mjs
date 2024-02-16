f7051681140efa53bf342ec3eab795a8ef4036f2
Francisco Olvera <francisco.olvera@mercadolivre.com>
2024-02-16 01:21:01 -0300

    try adapt @ paths and next file to mjs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+
+    return config;
+  },
+};
+
+export default nextConfig;
