54a9518e0265dc5153a89cd64f367579d4796e43
Francisco Olvera <francisco.olvera@mercadolivre.com>
2024-02-16 01:05:48 -0300

    try to use vercel analytics

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from 'react'
+import { Analytics } from '@vercel/analytics/react';
+ 
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       {children}
+      <Analytics />
     </div>
   )
 }
