4d831bf74a9f30d2df703de48723faebf032f80f
Francisco Olvera <francisco.olvera@mercadolivre.com>
2024-02-16 01:27:19 -0300

    try deploy without vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import { fileURLToPath } from "url";
-import path from "path";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -21,14 +13,6 @@ const nextConfig = {
         pathname: "**",
       },
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-
-    return config;
   },
 };
 
